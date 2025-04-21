8f22c3508a0dfa442b52247a9b866a7baf174c70
Naman Gupta <73887639+Namangupta123@users.noreply.github.com>
2025-04-21 19:54:38 +0530

    Added comments

--- config.py
+++ config.py
@@ -4,8 +4,7 @@ import streamlit as st
 from dotenv import load_dotenv
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
-'''
+RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")'''
 
 GROQ_API_KEY = st.secrets.GROQ_API_KEY
 RAPIDAPI_KEY = st.secrets.RAPIDAPI_KEY
