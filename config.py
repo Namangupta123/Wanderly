ad92bd64aa547aab0cf2a4bddb673c7130329624
NamanGupta123 <namangupta0330@gmail.com>
2025-04-21 19:50:11 +0530

    Added Warning

--- config.py
+++ config.py
@@ -4,10 +4,7 @@ import streamlit as st
 from dotenv import load_dotenv
 load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
-RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")
-TRAVEL=os.getenv("TRAVEL")
-FOOD=os.getenv("FOOD")
-STAY=os.getenv("STAY")'''
+RAPIDAPI_KEY = os.getenv("RAPIDAPI_KEY")'''
 
 GROQ_API_KEY = st.secrets.GROQ_API_KEY
 RAPIDAPI_KEY = st.secrets.RAPIDAPI_KEY
